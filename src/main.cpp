dbfb6b087c0c24ad240a8addcfa674208c230f0b
Igor Alimov <alimov@shturmann.com>
2013-04-11 12:24:00 +0400

    Удалено использование библиотеки gmp

--- src/main.cpp
+++ src/main.cpp
@@ -9,7 +9,6 @@
 #endif
 #include "gettext.h"
 #define _(string) gettext(string)
-#include <gmpxx.h>
 #include <iostream>
 #include <iomanip>
 #include <cstdlib>
@@ -24,28 +23,6 @@ try
     bindtextdomain(PACKAGE, LOCALEDIR);
     textdomain(PACKAGE);
     std::cout << _("Hello World !") << std::endl;
-    mpz_class a("12345678901234567890232346458769879783523523455465675674674");
-    mpz_class b("-56789012345678901234567890546458769780784562456347648568976456547567");
-    std::cout << _("First term: ") << a << std::endl;
-    std::cout << _("Second term: ") << b << std::endl;
-    std::cout << _("Sum: ") << a + b  << std::endl;
-    std::cout << _("Absolute value: ") << abs(a + b) << std::endl;
-    mpq_class d(2);
-    mpq_class e(3);
-    std::cout << _("Dividend: ") << d << std::endl;
-    std::cout << _("Divisor: ") << e << std::endl;
-    std::cout << _("Quotient: ") << d / e << std::endl;
-    mpq_class a1("3/5");
-    mpq_class b1("7/8");
-    std::cout << _("First term: ") << a1 << std::endl;
-    std::cout << _("Second term: ") << b1 << std::endl;
-    std::cout << _("Sum: ") << a1 + b1 << std::endl;
-    mpf_class d1(_("2.34123432453464575678876897978978987e12345"), 1000);
-    mpf_class e1(_("3.456457568768964634645756783567356756e45235"), 1000);
-    std::cout << std::setprecision(100);
-    std::cout << _("First multiplier: ")<< d1 << std::endl;
-    std::cout << _("Second multiplier: ") << e1 << std::endl;
-    std::cout << _("Product: ") << d1 * e1 << std::endl;
 	return EXIT_SUCCESS;
 }
 catch(const std::exception& er)
